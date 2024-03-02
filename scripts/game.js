286e5a847bb414eef505a6afe78963aa64761fb8
AliAbdelAall <3laych@gmail.com>
2024-03-02 19:15:18 +0200

    added event listener to display correct letters

--- scripts/game.js
+++ scripts/game.js
@@ -18,7 +18,7 @@ const word_list = [
 ];
 
 const random_word = word_list[Math.floor(Math.random() * word_list.length)];
-console.log(random_word);
+// console.log(random_word);
 
 const word_div = document.getElementById("answer-section");
 word_div.style.gap = "20px";
@@ -29,17 +29,20 @@ let under_letter;
 
 for (let i = 0; i < random_word.length; i++) {
   letter_wrapper = document.createElement("div");
+  letter_wrapper.className = "letter_wrapper"
   letter_wrapper.style.display = "flex";
   letter_wrapper.style.flexDirection = "column"
   letter_wrapper.style.width = "5%";
 
   letter = document.createElement("div");
+  letter.className = "answer-letter"
   letter.innerText = random_word[i].toUpperCase();
   letter.style.display = "flex";
   letter.style.justifyContent = "center";
   letter.style.visibility = "hidden"
 
   under_letter = document.createElement("div");
+  under_letter.className = "under-letter"
   under_letter.style.display = "inline-block";
   under_letter.style.height = "4px";
   under_letter.style.background = "#303030";
@@ -49,3 +52,31 @@ for (let i = 0; i < random_word.length; i++) {
   letter_wrapper.append(letter, under_letter);
   word_div.append(letter_wrapper);
 }
+
+const letters = document.querySelectorAll(".answer-letter")
+const under = document.querySelectorAll(".under-letter")
+
+const clicked_letter = document.querySelectorAll(".letter")
+
+let tries_count = 6;
+
+clicked_letter.forEach((element, index) => {
+  element.addEventListener("click", (event) => {
+    const letter = element.innerText
+    let found = false
+    for (let i = 0; i < random_word.length; i++) {
+      if (random_word[i].toUpperCase() === letter) {
+        letters[i].style.visibility = "visible";
+        under[i].style.visibility = "hidden";
+        found = true;
+      }
+    }
+    if (found) {
+      element.style.background = "green"
+    } else {
+
+      element.style.background = "red"
+    }
+  });
+});
+
